89cd808a3dfef27166445459e513ffebb474241d
Samuel Mwaura <mwauras@lafayette.edu>
2018-10-03 22:48:33 -0400

    UI change commit

--- VSCADA/main.cpp
+++ VSCADA/main.cpp
@@ -5,6 +5,25 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QApplication::setStyle(QStyleFactory::create("fusion"));
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(70,70,70));
+    palette.setColor(QPalette::WindowText, QColor(255,255,255));
+    palette.setColor(QPalette::Base, QColor(15,15,15));
+    palette.setColor(QPalette::AlternateBase, QColor(70,70,70));
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::yellow);
+    palette.setColor(QPalette::Button, QColor(70,70,70));
+    palette.setColor(QPalette::ButtonText, QColor(255,255,255));
+    palette.setColor(QPalette::BrightText, Qt::red);
+
+    palette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+    QApplication::setPalette(palette);
+
     MainWindow w;
     w.setWindowState((w.windowState()^Qt::WindowFullScreen));
     datapoint d;
